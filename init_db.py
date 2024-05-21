e4981fee00abefa0b8c001614ac03fb7713d6820
vonrodalars <lars@von-roda.de>
2024-05-21 14:39:55 +0200

    Update db init

--- init_db.py
+++ init_db.py
@@ -1,15 +1,15 @@
 from app import app
-from models import db, Bearbeiter, Customer, Ticket
+from models import db, User, Customer, Ticket
 
 # Create all the tables
 with app.app_context():
     db.create_all()
 
     # Add example data
-    bearbeiter1 = Bearbeiter(name="Max Mustermann")
-    bearbeiter2 = Bearbeiter(name="Erika Mustermann")
-    db.session.add(bearbeiter1)
-    db.session.add(bearbeiter2)
+    user1 = User(name="Max Mustermann")
+    user2 = User(name="Erika Mustermann")
+    db.session.add(user1)
+    db.session.add(user2)
 
     customer1 = Customer(name="John Doe")
     customer2 = Customer(name="Jane Doe")
